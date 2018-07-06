8164710edc261ec10400888cab98fcff6f80e191
Sparrow.jang <sparrow.jang@gmail.com>
2018-07-06 15:38:01 +0800

    Use $set and $unset to update session

--- flask_mongo_session/session_processor.py
+++ flask_mongo_session/session_processor.py
@@ -11,6 +11,63 @@ class Session(dict, SessionMixin):
 
     def get_id(self): return self.__id
 
+    __modified_keys = set()
+ 
+    def __call_original_method(self, name, *args, **kw):
+        return getattr(super(Session, self), name)(*args, **kw)
+ 
+ 
+    def __get_iterator(self, dictionary):
+        if dictionary.items:
+            it = dictionary.items()
+        else:
+            it = dictionary.iteritems()
+ 
+        return it
+ 
+    def clear(self, *args, **kw):
+        rv = self.__call_original_method('clear', *args, **kw)
+        for key in self.keys(): self.__modified_keys.add(key)
+        return rv
+ 
+    def popitem(self, *args, **kw):
+        rv = self.__call_original_method('popitem', *args, **kw)
+        self.__modified_keys.add(rv[0])
+        return rv
+ 
+    def update(self, *args, **kw):
+        rv = self.__call_original_method('update', *args, **kw)
+        it = self.__get_iterator(kw)
+ 
+        for key,  value in it: 
+            self.__modified_keys.add(key)
+ 
+        for arg in args:
+            if isinstance(arg, dict):
+                it = self.__get_iterator(arg)
+                for key,  value in it: 
+                    self.__modified_keys.add(key)
+ 
+        return rv
+ 
+ 
+    def __setitem__(self, *args, **kw):
+        rv = self.__call_original_method('__setitem__', *args, **kw)
+        self.__modified_keys.add(args[0])
+        return rv
+ 
+    def __delitem__(self, *args, **kw):
+        rv = self.__call_original_method('__delitem__', *args, **kw)
+        self.__modified_keys.add(args[0])
+        return rv
+
+    def modified_keys(self):
+        return [key for key in self.__modified_keys]
+
+    def reset_modified_keys(self):
+        self.__modified_keys.clear()
+
+
 class MongoSessionProcessor(object):
 
     def __init__(self, host):
@@ -32,14 +89,32 @@ class MongoSessionProcessor(object):
 
     def save_session(self, app, session, response):
         session_id = session.get_id()
+        unset_query = dict()
+        set_query = dict()
+        doc = dict()
 
-        if len(session.keys()):
-            doc = {'$set':session}
-        else:
-            doc = session
-            
-        self.__db.Session.update(dict(_id = session_id), doc, upsert = True)
+        for key in session.modified_keys():
+
+            value = session.get(key)
+
+            if value is None:
+                unset_query[key] = True
+
+            else:
+                set_query[key] = value
+
+        if len(unset_query.keys()):
+            doc['$unset'] = unset_query
+
+        if len(set_query.key()):
+            doc['$set'] = set_query
+
+        if len(unset_query.keys() + set_query.keys()): self.__db.Session.update(dict(_id = session_id), doc, upsert = True)
+
+        session.reset_modified_keys() 
+
         response.set_cookie(app.session_cookie_name, session_id)
+
 
     def is_null_session(self, session):
         return False

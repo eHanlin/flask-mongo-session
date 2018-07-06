a2d1ca8ff3107bfa37d28d0fd0477cc82df2bf59
Sparrow.jang <sparrow.jang@gmail.com>
2018-07-06 11:49:12 +0800

    Release 1.0.1 version

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ try:
 except:    
     from distutils.core import setup, find_packages
  
-setup(name='flask_mongo_session',
-      version='1.0.0',
+setup(name='flask-mongo-session',
+      version='1.0.1',
       description='Use mongo db as session.',
       author='Sparrow Jang',
       author_email='sparrow.jang@gmail.com',
